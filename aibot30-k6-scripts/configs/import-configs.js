1f2ab07a454f4f759f17d777372e475921db4a30
박수현 <soohyun.park@kt.com>
2024-05-14 13:06:49 +0900

    import config에 에러 로그 추가

--- aibot30-k6-scripts/configs/import-configs.js
+++ aibot30-k6-scripts/configs/import-configs.js
@@ -1,3 +1,10 @@
+/**
+ * 테스트 종류에 맞는 config를 import한다.
+ */
 
-console.log(`RUNNING ${__ENV.CUSTOM_K6_TEST_TYPE}`);
+const testTypes = ['average-load-test', 'breakpoint-test', 'smoke-test', 'soak-test', 'stress-test'];
+if(!testTypes.includes(__ENV.CUSTOM_K6_TEST_TYPE)) {
+    console.error(`Test type ${__ENV.CUSTOM_K6_TEST_TYPE} is not supported. Test type should be one of  ['average-load-test', 'breakpoint-test', 'smoke-test', 'soak-test', 'stress-test']`)
+}
+
 export const config = JSON.parse(open(`./${__ENV.CUSTOM_K6_TEST_TYPE}-config.json`));
